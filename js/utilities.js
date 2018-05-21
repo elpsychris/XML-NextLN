6cd78a36a126d33c0d72026b02d0d2f9edb094fd
Tung Nguyên <rohirrim3105@gmail.com>
2018-05-21 22:08:12 +0700

    add tab functions

--- js/utilities.js
+++ js/utilities.js
@@ -1,7 +1,21 @@
-changeTab = (tabId, childId) => {
-    if (typeof (Storage) !== 'undefined') {
-        localStorage.getItem(tabId + "_" + childId)
-    } else {
-        alert("Sorry! This site does not supported by this browser")
+changeTab = (tabEl, childId) => {
+    removeClassFromClassGroup("focus","tab-button")
+    tabEl.classList.add("focus")
+    changeTabContent(childId)
+}
+
+changeTabContent = (childId) => {
+    removeClassFromClassGroup("show","tab-content")
+    var allTabContent = document.getElementsByClassName("tab-content")       
+    allTabContent[childId].classList.add("show")
+}
+
+removeClassFromClassGroup = (kw, className) => {
+    var allEl = document.getElementsByClassName(className)
+    for (i = 0; i < allEl.length; i++) {
+        let curEl = allEl[i]
+        if (curEl.classList.contains(kw)) {
+            curEl.classList.remove(kw)
+        }
     }
 }
