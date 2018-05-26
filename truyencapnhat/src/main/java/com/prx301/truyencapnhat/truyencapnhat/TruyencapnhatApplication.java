aa8b39655af9ad3975cf6416c19eb514b21717a1
Tung Nguyên <rohirrim3105@gmail.com>
2018-05-26 09:02:15 +0700

    add backend prj

---
+++ truyencapnhat/src/main/java/com/prx301/truyencapnhat/truyencapnhat/TruyencapnhatApplication.java
@@ -0,0 +1,12 @@
+package com.prx301.truyencapnhat.truyencapnhat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruyencapnhatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruyencapnhatApplication.class, args);
+	}
+}
