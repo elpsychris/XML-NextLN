8777ff3ccb76eabbc172b95bc3c8742fb3b79aa3
Tung Nguyên <rohirrim3105@gmail.com>
2018-07-01 19:58:25 +0700

    add prefix and remove default ns

--- truyencapnhat/src/main/java/com/prx301/finalproject/truyencapnhat/model/crawler/model/package-info.java
+++ truyencapnhat/src/main/java/com/prx301/finalproject/truyencapnhat/model/crawler/model/package-info.java
@@ -1,6 +1,11 @@
 @XmlSchema(elementFormDefault = XmlNsForm.QUALIFIED,
-        namespace = "http://t3.com/2018/xml/config")
+        namespace = "http://t3.com/2018/xml/config",
+        xmlns = {
+                @XmlNs(prefix = "p",
+                        namespaceURI = "http://t3.com/2018/xml/config"),
+        })
 package com.prx301.finalproject.truyencapnhat.model.crawler.model;
 
+import javax.xml.bind.annotation.XmlNs;
 import javax.xml.bind.annotation.XmlNsForm;
 import javax.xml.bind.annotation.XmlSchema;
